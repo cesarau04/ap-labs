f63b17875b57eabbb38abf0652075389434c51dd
César Augusto <cesarau04@gmail.com>
2019-01-15 20:01:18 -0600

    Update chal1.py

--- challenges/chal1.py
+++ challenges/chal1.py
@@ -1,1 +1,13 @@
-print()
+s = "pwwkew"
+largest = ""
+ph = ""
+for i in list(s):
+  if i not in ph:
+  	ph+=i
+  else:
+  	if len(ph) > len(largest):
+  		largest = ph
+  		ph = "" + i 
+
+print(largest)
+print(len(largest))
