6e03fc0756e91f720b67a3b3ed65aa5aa51560e2
César Augusto <cesarau04@gmail.com>
2019-01-19 14:50:36 -0600

    Create exercise_1.11.c

---
+++ labs/lab0.1/exercise_1.11.c
@@ -0,0 +1,2 @@
+1)To test the program I would try to give as many different inputs as I can with differente scenarios, rare symbols, not finished lines, exiting program, one big word, a lot of tabs, multiline, etc.
+2)Inputs that are very strange or that doesn't permit the program to finished at all.
