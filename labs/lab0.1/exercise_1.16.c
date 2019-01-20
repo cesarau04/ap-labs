e29adaf7822f1a65897efa7a48660087e67a9ef1
César Augusto <cesarau04@gmail.com>
2019-01-20 14:54:25 -0600

    Create exercise_1.16.c

---
+++ labs/lab0.1/exercise_1.16.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#define MAXLINE 10
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main(){
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+
+    max = 0;
+    while ((len = getline(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+    }
+    if (max> 0)
+        printf("Termina con: %s", longest);
+        printf("Tamaño: %d\n", max);
+    return 0;
+}
+
+int getline(char s[], int lim){
+    int c, i, len_counter;
+    
+    c = 0;
+    len_counter = 0;
+    while(c != EOF){
+        if (c == '\n')
+            break;
+        for (i=0; i<lim-1 && (c=getchar()) !=EOF && c != '\n'; ++i){
+            s[i] = c;
+            ++len_counter;
+        }
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return len_counter;
+}        
+
+void copy(char to[], char from[]){
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
