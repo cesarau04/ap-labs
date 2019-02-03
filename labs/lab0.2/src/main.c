92118dd434f1d21823013e3a6b1755cf76acc037
cesar augusto garcia perez <cesarau04@gmail.com>
2019-02-02 19:47:41 -0600

    remove unused variables and defined all functions.

--- labs/lab0.2/src/main.c
+++ labs/lab0.2/src/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int strlen(char *str);
+char *stradd(char *origin, char *addition);
+int strfind(char *origin, char *substr);
 
 int main(int argc, char **argv)
 {
 	int result = strlen(&*argv[1]);
 	printf("Lenght: %d\n", result);
-	char *newstring;
 	stradd(&*argv[1], &*argv[2]);
 	printf("Concatenation: %s\n", argv[1]);
 	printf("Substring found -> ");
