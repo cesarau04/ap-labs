6b0bc0c90756c52a19163e5881bd6903412654fe
César Augusto <cesarau04@gmail.com>
2019-02-03 19:40:34 -0600

    Update maps.go

--- labs/lab0.3/maps.go
+++ labs/lab0.3/maps.go
@@ -6,8 +6,11 @@ import (
 )
 
 func WordCount(s string) map[string]int {
+    //Crea y prepara el map para poder hacer inserciones
     word_map := make(map[string]int)
+    //Itera atraves del array/slice.
     for _ ,word:= range strings.Fields(s){
+        //Crea la entrada. Anteriormente fueron las entradas creadas con 0.
         word_map[word]++
     }
     return word_map
