5f3f34b5675518cbed8350e803fd427c27f0c97c
cesar augusto garcia perez <cesarau04@gmail.com>
2019-02-05 19:12:15 -0600

    remove redundancy

--- labs/lab0.2/src/main.c
+++ labs/lab0.2/src/main.c
@@ -6,12 +6,12 @@ int strfind(char *origin, char *substr);
 
 int main(int argc, char **argv)
 {
-	int result = strlen(&*argv[1]);
+	int result = strlen(argv[1]);
 	printf("Lenght: %d\n", result);
-	stradd(&*argv[1], &*argv[2]);
+	stradd(argv[1], argv[2]);
 	printf("Concatenation: %s\n", argv[1]);
 	printf("Substring found -> ");
-	if (strfind(&*argv[1], &*argv[3])) {
+	if (strfind(argv[1], argv[3])) {
 		printf("True\n");
 	} else {
 		printf("False\n");
