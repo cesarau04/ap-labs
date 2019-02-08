2a79dfb62d69c1da1295ee39a2be35b0c3ca8d04
cesar augusto garcia perez <cesarau04@gmail.com>
2019-02-08 10:58:51 -0600

    First version is working. TODO:Need to free memory

--- labs/lab1.1/src/mycat.c
+++ labs/lab1.1/src/mycat.c
@@ -2,47 +2,57 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define ERROR_CODE 1
 #define SUCCESS_CODE 0
 #define MAX_SIZE 128
 void printUsage();
 
-int main(int argc, char **argv){
-	int fd;
+int main(int argc, char **argv)
+{
+	int fd, fd2;
 
-	if (argc == 2){
+	if (argc == 2) {
 		char buff[MAX_SIZE];
 		fd = open(argv[1], O_RDONLY);
-		if (fd == -1){
+		if (fd == -1) {
 			printf("Couldn't open file\n");
 			return ERROR_CODE;
 		}
 
-		while(read(fd, buff, MAX_SIZE-1) != 0){
+		while (read(fd, buff, MAX_SIZE - 1) != 0) {
 			buff[MAX_SIZE] = '\0';
-			printf("%s",buff);
+			printf("%s", buff);
 		}
-		printf("\n");
 		close(fd);
-	}else if (argc == 4) {
-	
-	}else{
-		printUsage();	
+	} else if (argc == 4) {
+		if (strcmp(argv[2], "-c") == 0) {
+			char *buff;
+			buff = calloc(MAX_SIZE, sizeof(char)); 
+			fd = open(argv[1], O_RDONLY);
+			fd2 = open(argv[3], O_WRONLY | O_CREAT, S_IRWXU);
+			if (fd == -1){
+				printf("Couldn't open %s", argv[1]);
+				close(fd);
+				printUsage();
+				return ERROR_CODE;
+			}
+			while (read(fd, buff, MAX_SIZE -1) != 0){
+				write(fd2, buff, MAX_SIZE-1);
+			}
+			close(fd);
+			close(fd2);
+		}
+	} else {
+		printUsage();
 	}
-
-	/*
-	if (strcmp(argv[2], "-c") == 0){
-		printf("Copy\n");
-	}else{
-		printf("To terminal\n");
-	}
-	*/
 	return SUCCESS_CODE;
 }
 
-void printUsage(){
+void printUsage()
+{
 	printf("./mycat file [FLAGS] [newfile]\n\nFLAGS:\n"
-			"-r\t\t--Read and print to terminal. [DEFAULT]\n"
-			"-c\t\t--Copy content to a new file.\n");
+	       "  \t\t--Read and print to terminal. [DEFAULT]\n"
+	       "-c\t\t--Copy content to a new file.\n");
 }
