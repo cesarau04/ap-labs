a26d42a4f9a9dbd9dda4ac727da321031e4aa472
cesar augusto garcia perez <cesarau04@gmail.com>
2019-02-10 00:48:49 -0600

    formating

--- labs/lab1.2/src/broadcast.c
+++ labs/lab1.2/src/broadcast.c
@@ -11,35 +11,36 @@
 
 void usage();
 
-int main(int argc, char **argv){
-	if (argc <= 1){
+int main(int argc, char **argv)
+{
+	if (argc <= 1) {
 		usage();
 	}
 
 	size_t i;
 	size_t msglen = 0;
-	for (i = 1; i < argc; i++){
-		msglen += strlen(argv[i])+1; 
-	} 
+	for (i = 1; i < argc; i++) {
+		msglen += strlen(argv[i]) + 1;
+	}
 	char *msg;
 	msg = calloc(msglen, sizeof(char));
 	msg[0] = '\0';
-	for (i = 1; i < argc; i++){
+	for (i = 1; i < argc; i++) {
 		strcat(msg, argv[i]);
 		strcat(msg, " ");
 	}
-	msg[msglen-1] = '\n';
+	msg[msglen - 1] = '\n';
 
-	DIR *dir; 
+	DIR *dir;
 	struct dirent *pDirent;
 	static char *path = "/dev/pts/";
-	int errnum; 
+	int errnum;
 
 	const int pathlength = strlen(path);
 
 	dir = opendir(path);
-	
-	if (dir == NULL){
+
+	if (dir == NULL) {
 		perror("Error directory: ");
 		return EXIT_FAILURE;
 	}
@@ -49,32 +50,34 @@ int main(int argc, char **argv){
 	int flag = 0;
 	int len, fd;
 	i = 0;
-	while ((pDirent = readdir(dir)) != NULL){
+	while ((pDirent = readdir(dir)) != NULL) {
 		len = strlen(pDirent->d_name);
-		
-		for (c = pDirent->d_name[i]; i<len; i++){
-			if (isdigit(c) != 0){
+
+		for (c = pDirent->d_name[i]; i < len; i++) {
+			if (isdigit(c) != 0) {
 				continue;
-			}else{
+			} else {
 				flag = -1;
 				break;
 			}
 		}
-		if (flag == -1){
+		if (flag == -1) {
 			flag = 0;
 			continue;
 		}
-		
-		if ((pathpts = calloc((strlen(pDirent->d_name)+pathlength) + 1, sizeof(char))) == NULL){
+
+		if ((pathpts =
+		     calloc((strlen(pDirent->d_name) + pathlength) + 1,
+			    sizeof(char))) == NULL) {
 			perror("Error calloc: ");
-		}else{
+		} else {
 			pathpts[0] = '\0';
 			strcat(pathpts, path);
 			strcat(pathpts, pDirent->d_name);
 		}
-		
+
 		fd = open(pathpts, O_WRONLY | O_NOCTTY);
-		if (fd == -1){
+		if (fd == -1) {
 			perror("Error file descriptor: ");
 		}
 		write(fd, msg, msglen);
@@ -85,8 +88,9 @@ int main(int argc, char **argv){
 	return EXIT_SUCCESS;
 }
 
-void usage(){
-	printf(
-			"Send a message to all pts terminals connected in the computer.\n\n"
-			"Usage: ./broadcast [message]\n");
+void usage()
+{
+	printf
+	    ("Send a message to all pts terminals connected in the computer.\n\n"
+	     "Usage: ./broadcast [message]\n");
 }
