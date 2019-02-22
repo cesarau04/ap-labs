8755dc934e103bf798312e261fd2c20d95f300e2
cesar augusto garcia perez <cesarau04@gmail.com>
2019-02-22 11:29:37 -0600

    Working with out punctioation

--- labs/lab2.3/cross-ref.c
+++ labs/lab2.3/cross-ref.c
@@ -7,38 +7,56 @@
 #include <string.h>
 
 #define BUFF_SIZE 512
-int main(int argc, char **argv){
-	if (argc != 2){ 
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
 		printf("Usage: ./cross-ref [file.txt]\n");
 		return 1;
 	}
 
 	int fd = open(argv[1], O_RDONLY);
-	if (fd == -1){
+	if (fd == -1) {
 		perror("Error opening file");
 	}
 
 	char *buff;
-	buff = (char *) calloc(BUFF_SIZE, sizeof(char));
-	// memset(buff, '\0', BUFF_SIZE);
+	buff = (char *)calloc(BUFF_SIZE, sizeof(char));
 
 	char *line;
-	line = (char *) calloc(BUFF_SIZE, sizeof(char));
-	// memset(line, '\0', BUFF_SIZE);
+	line = (char *)calloc(BUFF_SIZE, sizeof(char));
 
+	char *token;
+
+	char separator[2] = " ";
 	off_t offset = 0;
-	size_t iter = 1;
-	while (read(fd, buff, BUFF_SIZE-1)){
+	while (read(fd, buff, BUFF_SIZE - 1)) {
 		// get line
 		int i;
-		for (i = 0; buff[i] != '\n'; ++i){
-			line[i] = buff[i];		
+		for (i = 0; buff[i] != '\n'; ++i) {
+			line[i] = buff[i];
 		}
 		line[i] = '\0';
-		offset += (i+1);
+		offset += (i + 1);
 		lseek(fd, offset, SEEK_SET);
-		printf("%ld %s\n", iter, line);
-		++iter;
+
+		token = strtok(line, separator);
+
+		while (token != NULL) {
+			i = strlen(token);
+			if (token[i-1] == '.' || token[i-1] == ',' || token[i-1] == ':' || token[i-1] == ';' || token[i-1] == ')' ){
+				token[i-1] = '\0';
+			}
+			printf("%s ", token);
+			token = strtok(NULL, separator);
+		}
+		printf("\n");
 	}
+
 	return 0;
 }
+
+
+
+
+
