23b4366312b8a5b0152e5caf3ad9e52236127dc8
cesar augusto garcia perez <cesarau04@gmail.com>
2019-03-17 19:05:08 -0600

    formating, and working fine

--- labs/lab3.1/monitor.c
+++ labs/lab3.1/monitor.c
@@ -15,76 +15,77 @@ int inotifyFd;
 
 static int
 get_files(const char *fpath, const struct stat *sb,
-	     int tflag, struct FTW *ftwbuf)
+	  int tflag, struct FTW *ftwbuf)
 {
 	/* Check for creation, deletion or rename of files */
-	int wd = inotify_add_watch(inotifyFd, fpath, IN_CREATE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO);
-  if (wd == -1){
+	int wd = inotify_add_watch(inotifyFd, fpath,
+				   IN_CREATE | IN_DELETE | IN_MOVED_FROM |
+				   IN_MOVED_TO);
+	if (wd == -1) {
 		errorf("couldn't add to inotify_add_watch");
 		exit(EXIT_FAILURE);
 	}
 	return 0;		/* To tell nftw() to continue */
 }
 
-static void             /* Display information from inotify_event structure */
+static void /* Display information from inotify_event structure */
 displayInotifyEvent(struct inotify_event *i)
 {
-    printf("    wd =%2d; ", i->wd);
-    if (i->cookie > 0)
-        printf("cookie =%4d; ", i->cookie);
+	if (i->mask & IN_CREATE)
+		infof("IN_CREATE ");
+	if (i->mask & IN_DELETE)
+		infof("IN_DELETE ");
+	if (i->mask & IN_MOVED_FROM)
+		infof("IN_MOVED_FROM ");
+	if (i->mask & IN_MOVED_TO)
+		infof("IN_MOVED_TO ");
+	printf("\n");
 
-    printf("mask = ");
-    if (i->mask & IN_CREATE)        printf("IN_CREATE ");
-    if (i->mask & IN_DELETE)        printf("IN_DELETE ");
-    if (i->mask & IN_MOVED_FROM)    printf("IN_MOVED_FROM ");
-    if (i->mask & IN_MOVED_TO)      printf("IN_MOVED_TO ");
-    printf("\n");
-
-    if (i->len > 0)
-        printf("        name = %s\n", i->name);
+	if (i->len > 0)
+		printf("        name = %s\n", i->name);
 }
+
 int main(int argc, char *argv[])
 {
 	inotifyFd = inotify_init();
-	if (inotifyFd == -1){
+	if (inotifyFd == -1) {
 		errorf("couldn't create inotifyFd");
 		exit(EXIT_FAILURE);
 	}
 
-	int flags = FTW_PHYS; /* Don't follow symbolic links */
-	if (nftw((argc < 2) ? "." : argv[1], get_files, 20, flags)
-	    == -1) {
+	int flags = FTW_PHYS;	/* Don't follow symbolic links */
+	if (nftw((argc < 2) ? "." : argv[1], get_files, 20, flags) == -1) {
 		panicf("couldn't transverse nftw");
 		exit(EXIT_FAILURE);
 	}
 
-  char buf[BUF_LEN] __attribute__ ((aligned(8)));
-  ssize_t numRead;
-  char *p;
-  struct inotify_event *event;
+	char buf[BUF_LEN] __attribute__ ((aligned(8)));
+	ssize_t numRead;
+	char *p;
+	struct inotify_event *event;
 
-	for (;;) {                                  /* Read events forever */
-        numRead = read(inotifyFd, buf, BUF_LEN);
-        if (numRead == 0){
-            panicf("read() from inotify fd returned 0!");
-						exit(EXIT_FAILURE);
-				}
-        if (numRead == -1){
-            errorf("read");
-						exit(EXIT_FAILURE);
-				}
-        infof("Read %ld bytes from inotify fd\n", (long) numRead);
-        for (p = buf; p < buf + numRead; ) {
-            event = (struct inotify_event *) p;
-            displayInotifyEvent(event);
-            p += sizeof(struct inotify_event) + event->len;
-        }
-				inotifyFd = inotify_init();
-				if (nftw((argc < 2) ? "." : argv[1], get_files, 20, flags)
-				    == -1) {
-					panicf("couldn't transverse nftw");
-					exit(EXIT_FAILURE);
-				}
-  }
+	for (;;) {		/* Read events forever */
+		numRead = read(inotifyFd, buf, BUF_LEN);
+		if (numRead == 0) {
+			panicf("read() from inotify fd returned 0!");
+			exit(EXIT_FAILURE);
+		}
+		if (numRead == -1) {
+			errorf("read");
+			exit(EXIT_FAILURE);
+		}
+		// infof("Read %ld bytes from inotify fd\n", (long) numRead);
+		for (p = buf; p < buf + numRead;) {
+			event = (struct inotify_event *)p;
+			displayInotifyEvent(event);
+			p += sizeof(struct inotify_event) + event->len;
+		}
+		inotifyFd = inotify_init();
+		if (nftw((argc < 2) ? "." : argv[1], get_files, 20, flags) ==
+		    -1) {
+			panicf("couldn't transverse nftw");
+			exit(EXIT_FAILURE);
+		}
+	}
 	exit(EXIT_SUCCESS);
 }
