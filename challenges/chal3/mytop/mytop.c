4ae41738babdadf12d87c697cb1f350a36aff015
cesar augusto garcia perez <cesarau04@gmail.com>
2019-03-27 23:14:35 -0600

    Just need to update from status file

--- challenges/chal3/mytop/mytop.c
+++ challenges/chal3/mytop/mytop.c
@@ -7,10 +7,21 @@
 #include <ctype.h>
 #include <string.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define PROC_PATH     "/proc/"
+#define FD_PATH       "/fd/"    //PROC_PATH + PID + FD_PATH
+#define MEMORY_PATH   "/statm"  //PROC_PATH + PID + MEMORY_PATH
+#define STATUS_PATH   "/status" 
 #define NAMEFILE_SIZE 25
 #define STATE_SIZE    12
+#define MAX_PATH_SIZE 25
+#define INT_TO_STR_SIZE 11
+#define BUFF_SIZE     512
+
 
 void clear();
 bool isDirectory(unsigned char *type);
@@ -21,6 +32,7 @@ struct process *addnode(struct process *p, unsigned int *pid);
 unsigned int parseUnsigedInt(char * str);
 void treeprint(struct process *p);
 void *findInfoForProcess(void *argv);
+unsigned int countFds(char *path);
 
 struct process {
   unsigned int pid;
@@ -63,16 +75,18 @@ int main(){
 		root = addnode(root, &param);	
 	}
 
+	closedir(dir);
+
 	for(int i = 0; i < threadsopen; i++){
 		pthread_join(threadslist[i], NULL);	
 	}
 	//treeprint(root);
-  //clear();
+	//clear();
   return 0;
 }
 
 void clear() {
-  printf("\e[1;1H\e[2J"); 
+  printf("\e7"); 
 }
 
 bool isDirectory(unsigned char *type){
@@ -151,9 +165,81 @@ void treeprint(struct process *p)
 void *findInfoForProcess(void *param){
 	struct threadstruct* ts;
 	ts = (struct threadstruct *) param;
-	/* Here goes the search for info of each process */
+	/* Save the routes for the files */
+	char *mempath, *fdpath, *statuspath, *pidstr, *buff, *dataph;
+	mempath    = (char *) malloc(MAX_PATH_SIZE * sizeof(char));
+	statuspath = (char *) malloc(MAX_PATH_SIZE * sizeof(char));
+	fdpath     = (char *) malloc(MAX_PATH_SIZE * sizeof(char));
+	pidstr     = (char *) malloc(INT_TO_STR_SIZE * sizeof(char));
+	buff       = (char *) malloc(BUFF_SIZE * sizeof(char));
+	dataph     = (char *) malloc(NAMEFILE_SIZE * sizeof(char));  //use the largest posible value to store
+
+	sprintf(pidstr, "%u", ts->mynode->pid);
+	strcpy(mempath, PROC_PATH);
+	strcat(mempath, pidstr);     // /proc/<pid>
+	strcpy(fdpath, mempath);      
+	strcpy(statuspath, mempath);
+	strcat(mempath, MEMORY_PATH);
+	strcat(fdpath, FD_PATH);
+	strcat(statuspath, STATUS_PATH);
+	free(pidstr);	
+
+	// Read statm, for memory usage
+	int fd;
+	fd = open(mempath, O_RDONLY);
+	if (fd == -1){
+		printf("Couldn't open mempath: %s", mempath);
+		exit(EXIT_FAILURE);
+	}
+	// This one is fast, just read the until the first ' ' and save to memory utilization
+	int i = 0;
+	while (read(fd, buff, BUFF_SIZE) > 0){
+		for (; buff[i] != ' '; i++){
+			dataph[i] = buff[i];	
+		}		
+	}
+	dataph[i] = '\0';
+	ts->mynode->memory = parseUnsigedInt(dataph); 
+	//free(mempath);
+	close(fd);
+	
+	ts->mynode->no_openfd = countFds(fdpath);
+	//free(fdpath);
+
+	fd = open(statuspath, O_RDONLY);
+	if (fd == -1){
+		printf("Couldn't open statuspath: %s", statuspath);
+		exit(EXIT_FAILURE);
+	}
+	
+	i = 0;
+	dataph[0] = '\0'; //Is even necessary?
+	char *attr;
+	attr = (char *) malloc(NAMEFILE_SIZE*sizeof(char));
+	while (read(fd, buff, BUFF_SIZE) > 0) {
+		for (; buff[i] != '\n'; i++){
+			dataph[i] = buff[i];		
+		}
+		dataph[i] = '\0';
+	}
+	close(fd);
+	
 	return (void *) (size_t) ts->pid;
 }
 
-// ASLKJDFLAKSJ	
+unsigned int countFds(char *path){
+	unsigned int file_count = 0;
+	DIR *dir;
+	struct dirent *entry;
+
+	dir = opendir(path); 
+	if (dir == NULL){
+		perror("opendir(); line 216");
+		exit(EXIT_FAILURE);
+	}
+	while ((entry = readdir(dir)) != NULL)
+         file_count++;
+	closedir(dir);
+	return file_count - 2;
+}
 
