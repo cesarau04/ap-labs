8de971ae6924cd1ec5810ee53a5ac9b8e0798b72
cesar augusto garcia perez <cesarau04@gmail.com>
2019-04-21 21:55:48 -0500

    indeting

--- labs/lab4.2/multiplier.c
+++ labs/lab4.2/multiplier.c
@@ -8,23 +8,23 @@
 #include <pthread.h>
 #include "logger.h"
 
-long * readMatrix(char *filename);
-long * getColumn(int col, long *matrix);
-long * getRow(int row, long *matrix);
+long *readMatrix(char *filename);
+long *getColumn(int col, long *matrix);
+long *getRow(int row, long *matrix);
 int getLock(void);
 int releaseLock(int lock);
 long dotProduct(long *vec1, long *vec2);
-long * multiply(long *matA, long *matB);
+long *multiply(long *matA, long *matB);
 int saveResultMatrix(long *result);
 void *threadFunc(void *arg);
 
-int NUM_BUFFERS; 
-long **buffers; 
+int NUM_BUFFERS;
+long **buffers;
 pthread_mutex_t *mutexes;
-long *result; 
+long *result;
 pthread_t threads[2000];
 
-struct DataStruct{
+struct DataStruct {
 	long *matA;
 	long *matB;
 	size_t rowPosition;
@@ -34,19 +34,20 @@ struct DataStruct{
 
 int main(int argc, char **argv)
 {
-	if (argc != 3){
+	if (argc != 3) {
 		errorf("USAGE: ./multiplier -n NUMBER\n");
 		exit(EXIT_FAILURE);
 	}
 	NUM_BUFFERS = strtol(argv[2], NULL, 10);
 	infof("NUM_BUFFERS => %d\n", NUM_BUFFERS);
-	
+
 	// TODO: allocate inside buffers
-	buffers = (long **) malloc(NUM_BUFFERS * sizeof(long*));
-	mutexes = (pthread_mutex_t *) malloc(NUM_BUFFERS * sizeof(pthread_mutex_t));
+	buffers = (long **)malloc(NUM_BUFFERS * sizeof(long *));
+	mutexes =
+	    (pthread_mutex_t *) malloc(NUM_BUFFERS * sizeof(pthread_mutex_t));
 
 	// TODO: Need to check if this works 
-	for (int i = 0; i < NUM_BUFFERS; i++){
+	for (int i = 0; i < NUM_BUFFERS; i++) {
 		pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 		mutexes[i] = mutex;
 		pthread_mutex_init(&mutexes[i], NULL);
@@ -57,7 +58,7 @@ int main(int argc, char **argv)
 	long *matrixA, *matrixB;
 	matrixA = readMatrix("matA.dat");
 	matrixB = readMatrix("matB.dat");
-	
+
 	infof("matrices read\n");
 
 	result = multiply(matrixA, matrixB);
@@ -73,111 +74,121 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-long * readMatrix(char *filename){
+long *readMatrix(char *filename)
+{
 	FILE *fp;
 	char *line = NULL;
 	size_t len = 0;
 	fp = fopen(filename, "r");
-	if (fp == NULL){
+	if (fp == NULL) {
 		panicf("Couldn't open %s\n", filename);
 		return NULL;
 	}
 	long *matrix;
-	matrix = (long *) malloc(4000000 * sizeof(long));
+	matrix = (long *)malloc(4000000 * sizeof(long));
 	int i = 0;
-	while (getline(&line, &len, fp) != -1){
+	while (getline(&line, &len, fp) != -1) {
 		matrix[i++] = strtol(line, NULL, 10);
 	}
 	free(line);
 	return matrix;
 }
 
-long * getColumn(int col, long *matrix){
-	if (col < 0 || col > 2000){
+long *getColumn(int col, long *matrix)
+{
+	if (col < 0 || col > 2000) {
 		panicf("col must be 0>col>2000\n");
 		exit(EXIT_FAILURE);
 	}
-	size_t offset = col-1;
+	size_t offset = col - 1;
 	long *mycol;
-	mycol = (long *) malloc(2000 * sizeof(long));
+	mycol = (long *)malloc(2000 * sizeof(long));
 
-	for (int i = 0; i < 2000; i++){
+	for (int i = 0; i < 2000; i++) {
 		mycol[i] = matrix[offset];
 		offset += 2000;
 	}
 	return mycol;
 }
 
-long * getRow(int row, long *matrix){
-	if (row < 0 || row > 2000){
+long *getRow(int row, long *matrix)
+{
+	if (row < 0 || row > 2000) {
 		panicf("row must bm 0>row>2000\n");
 		exit(EXIT_FAILURE);
 	}
 	size_t offset = ((2 * row) - 2) * 1000;
 	long *myrow;
-	myrow = (long *) malloc(2000 * sizeof(long));
-	
-	for (int i = 0; i < 2000; i++){
+	myrow = (long *)malloc(2000 * sizeof(long));
+
+	for (int i = 0; i < 2000; i++) {
 		myrow[i] = matrix[offset++];
 	}
 	return myrow;
 }
 
-int getLock(void){
-	for (int i = 0; i < NUM_BUFFERS; i++){
+int getLock(void)
+{
+	for (int i = 0; i < NUM_BUFFERS; i++) {
 		if (pthread_mutex_trylock(&mutexes[i]) == 0)
 			return i;
 	}
 	return -1;
 }
 
-int releaseLock(int lock){
-	// TODO: Release the buffer	
-	if (pthread_mutex_unlock(&mutexes[lock]) == 0){
+int releaseLock(int lock)
+{
+	// TODO: Release the buffer     
+	if (pthread_mutex_unlock(&mutexes[lock]) == 0) {
 		return 0;
 	}
 	return -1;
 }
 
-long dotProduct(long *vec1, long *vec2){
+long dotProduct(long *vec1, long *vec2)
+{
 	long result = 0;
 	for (int i = 0; i < 2000; i++)
-			result += vec1[i]*vec2[i];
+		result += vec1[i] * vec2[i];
 	return result;
 }
 
-long * multiply(long *matA, long *matB){
-	result  = (long *) malloc(4000000 * sizeof(long));
-	
-	for (size_t i = 0; i < 2000; i++){
+long *multiply(long *matA, long *matB)
+{
+	result = (long *)malloc(4000000 * sizeof(long));
+
+	for (size_t i = 0; i < 2000; i++) {
 		// Create threads
-		for (size_t j = 0; j < 2000; j++){
+		for (size_t j = 0; j < 2000; j++) {
 			struct DataStruct *currentStruct;
-			currentStruct = (struct DataStruct *) malloc(sizeof(struct DataStruct));
+			currentStruct =
+			    (struct DataStruct *)
+			    malloc(sizeof(struct DataStruct));
 			currentStruct->matA = matA;
 			currentStruct->matB = matB;
 			currentStruct->rowPosition = i + 1;
 			currentStruct->colPosition = j + 1;
 			currentStruct->result = result;
-			pthread_create(&threads[j], NULL, threadFunc, (void *) currentStruct);
+			pthread_create(&threads[j], NULL, threadFunc,
+				       (void *)currentStruct);
 		}
-		
+
 		// Wait threads
 		for (size_t j = 0; j < 2000; j++)
-			pthread_join(threads[j], NULL);	
-		printf("\r%ld%%", (i*100)/1999);
-    fflush(stdout);
+			pthread_join(threads[j], NULL);
+		printf("\r%ld%%", (i * 100) / 1999);
+		fflush(stdout);
 	}
 	printf("\n");
 	return result;
 }
 
-int saveResultMatrix(long *result){
+int saveResultMatrix(long *result)
+{
 	FILE *f = fopen("result.dat", "w");
-	if (f == NULL)
-	{
-    errorf("error: couldn't open/create result.dat\n");
-    return -1;
+	if (f == NULL) {
+		errorf("error: couldn't open/create result.dat\n");
+		return -1;
 	}
 
 	for (size_t i = 0; i < 3999999; i++)
@@ -187,15 +198,16 @@ int saveResultMatrix(long *result){
 	return 0;
 }
 
-void *threadFunc(void *arg){
-	struct DataStruct *data = (struct DataStruct *) arg;
+void *threadFunc(void *arg)
+{
+	struct DataStruct *data = (struct DataStruct *)arg;
 	long *row, *col;
 	long index;
 
 	row = getRow(data->rowPosition, data->matA);
 	col = getColumn(data->colPosition, data->matB);
-	
-	index = ((((data->rowPosition - 1) * 2000) + data->colPosition) - 1); 
+
+	index = ((((data->rowPosition - 1) * 2000) + data->colPosition) - 1);
 	data->result[index] = dotProduct(row, col);
 
 	free(row);
@@ -203,7 +215,3 @@ void *threadFunc(void *arg){
 	free(arg);
 	return NULL;
 }
-
-
-
-
